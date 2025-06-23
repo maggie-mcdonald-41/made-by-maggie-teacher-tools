d2dedb6b91628aa9c7729afb45a595a81baeaa8c
Maggie McDonald <113684788+maggie-mcdonald@users.noreply.github.com>
2025-06-22 23:12:59 -0400

    Google API Configuration

---
+++ argumentative-organizer/js/config.js
@@ -0,0 +1,3 @@
+// config.js
+// Rename this file to config.js and insert your actual Google Client ID below
+const GOOGLE_CLIENT_ID = '592399844090-i5e5nc7a098as70j39cab8lsv8ini9t0.apps.googleusercontent.com';
