6c78bee84fdb9d0d35329a0f4dfc2c9645fd4344
Maggie McDonald <113684788+maggie-mcdonald@users.noreply.github.com>
2025-06-22 23:24:22 -0400

    Updated index

--- argumentative-organizer/js/google-integration.js
+++ argumentative-organizer/js/google-integration.js
@@ -2,7 +2,7 @@
    Google OAuth + Drive Autosave + Docs Export + Edit Logging */
 
 // 1) Constants & state
-const CLIENT_ID   = GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_ID = '592399844090-i5e5nc7a098as70j39cab8lsv8ini9t0.apps.googleusercontent.com';
 const SCOPES      = [
   'https://www.googleapis.com/auth/drive.file',
   'https://www.googleapis.com/auth/documents'
